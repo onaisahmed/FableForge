efe036abcd078aee8b74068d5f0a4b0c4a51d051
Taha Ghous <tahaghous2002@gmail.com>
2025-05-11 22:21:11 +0500

    created prompt class

--- src/main/java/fableforge/core/PromptRefiner.java
+++ src/main/java/fableforge/core/PromptRefiner.java
@@ -1,4 +1,7 @@
 package fableforge.core;
 
 public class PromptRefiner {
+    public static String createPrompt(String topic) {
+        return "Write a short story about this topic maximum 100 words: " + topic;
+    }
 }

d1fae9a802aa17373244bf456f4b42aecb3295a4
Taha Ghous <tahaghous2002@gmail.com>
2025-05-11 22:20:34 +0500

    created story generated class to call ai service

--- src/main/java/fableforge/core/StoreGenerator.java
+++ src/main/java/fableforge/core/StoreGenerator.java
@@ -1,4 +1,25 @@
 package fableforge.core;
 
+import fableforge.services.AiService;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class StoreGenerator {
+    public static String generateStory(String topic) {
+        String prompt = PromptRefiner.createPrompt(topic);
+        String response = AiService.callGemini(prompt);
+        return extractStoryFromJson(response);
+    }
+
+    private static String extractStoryFromJson(String json) {
+        JSONObject obj = new JSONObject(json);
+        JSONArray candidates = obj.getJSONArray("candidates");
+        JSONObject firstCandidate = candidates.getJSONObject(0);
+        JSONObject content = firstCandidate.getJSONObject("content");
+        JSONArray parts = content.getJSONArray("parts");
+        JSONObject part = parts.getJSONObject(0);
+        return part.getString("text");
+    }
 }
+

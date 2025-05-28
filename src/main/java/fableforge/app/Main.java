880ce28040eda1f1ac291ccf99d1fbfd5535d099
onaisahmed <onaisahmed02@gmail.com>
2025-05-29 01:29:43 +0500

    Updated no prompt text, removed comments

--- src/main/java/fableforge/app/Main.java
+++ src/main/java/fableforge/app/Main.java
@@ -16,28 +16,24 @@ public class Main extends Application {
         mainScreen.getButton().setOnAction(e -> {
             String userInput = mainScreen.getPromptBox().getText();
             if (userInput.trim().isEmpty()) {
-                mainScreen.getStoryBox().appendText("Please enter a prompt to generate Story!");
+                mainScreen.getStoryBox().clear();
+                mainScreen.getStoryBox().appendText("A prompt, please. Every story needs a start!");
                 return;
             }
             // Clearing previous story first [BUG fixed]
             mainScreen.getStoryBox().clear();
-
             // Generate story using your core logic
             String story = StoreGenerator.generateStory(userInput);
 
             // Display the generated story in the StoryDisplayBox
             mainScreen.getStoryBox().appendText(story);
-
-            // Clear the prompt input
-//            mainScreen.getPromptBox().clear();
         });
 
-        Scene scene = new Scene(mainScreen.getRoot(), 600, 600); // Adjust size as needed
+        Scene scene = new Scene(mainScreen.getRoot(), 600, 600);
         primaryStage.setTitle("FableForge Story Generator");
         // Disable resizing
         primaryStage.setResizable(false);
 
-        // Optional: set fixed size (if needed)
         primaryStage.setWidth(613);
         primaryStage.setHeight(637);
         primaryStage.setScene(scene);
@@ -45,6 +41,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);  // Starts JavaFX application
+        launch(args);
     }
 }
